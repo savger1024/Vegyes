20c55f5a0ebd260759af37ff5e304734c72408b2
savger1024 <90380477+savger1024@users.noreply.github.com>
2022-09-23 12:39:58 +0200

    "Penz" segédosztály (címlet darabszámhoz)
    Objektumban van tárolva a címlet és a darabszám
    Foreach-ben van a példányosítás, és hozzáadja egy objektumlistához

---
+++ C#/Hello/CimletFeladat/Penz.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GyakorlasCsharpKonzol0932
+{
+    class Penz
+    {
+        public int cimlet;
+        public int darab;
+        public Penz(int cimlet, int darab)
+        {
+            this.cimlet = cimlet;
+            this.darab = darab;
+        }
+    }
+}
